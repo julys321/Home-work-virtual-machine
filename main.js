0907b9697f07566b97e9ab609b0f2f35c8decfd9
Julius Lungys <juliuslungys@gmail.com>
2018-11-25 17:42:19 +0200

    add output as elemt in html

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ function main(){
     virtualMachine.program = binFile;
     //console.log(virtualMachine);
     virtualMachine.executeProgram();
+    document.getElementById("virtualMachine").innerHTML = "Output is:\n"+"<br>"+virtualMachine.output+"<br>";
     console.log("Output is:\n"+virtualMachine.output);
 }
